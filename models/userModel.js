a32f0172909229c0856495fef3267b233f9b4d3d
Karthik171819 <karthikkumar092004@gmail.com>
2025-10-29 15:42:08 +0530

    Model Creation and schema types

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
+//schema creation
 const  userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    email: String,
+    createdAt: Date,
+    updatedAt: Date,
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address: {
+        city: String,
+        street: string
+    }
 });
 
 const userModel = mongoose.model('User', userSchema);
